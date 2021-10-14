54e645c6995b5b8d14c67a12480e0343767cf0db
DuckLackn <89722816+DuckLackn@users.noreply.github.com>
2021-10-14 17:56:03 -0400

    Update MainScript.lua

--- Script Hub Scripts/MainScript.lua
+++ Script Hub Scripts/MainScript.lua
@@ -3,5 +3,5 @@ if game.PlaceId ~= 2377868063 then
     return
 end
 if game.PlaceId == 2377868063 then
-    loadstring(game:HttpGet"URL")
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DuckLackn/Script-Hub/main/Script%20Hub%20Scripts/Strucid%20Game.lua")
 end
