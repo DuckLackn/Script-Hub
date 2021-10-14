3d02f526603b80ee656716a09c08aef7df3d50f3
DuckLackn <89722816+DuckLackn@users.noreply.github.com>
2021-10-14 17:56:43 -0400

    Update MainScript.lua

--- Script Hub Scripts/MainScript.lua
+++ Script Hub Scripts/MainScript.lua
@@ -1,7 +1,7 @@
 if game.PlaceId ~= 2377868063 then
-    game:GetService("Players").LocalPlayer:Kick("ur not in jailbreak")
+    game:GetService("Players").LocalPlayer:Kick("This Isn't A Valid Game")
     return
 end
 if game.PlaceId == 2377868063 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/DuckLackn/Script-Hub/main/Script%20Hub%20Scripts/Strucid%20Game.lua")
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DuckLackn/Script-Hub/main/Script%20Hub%20Scripts/Strucid%20Game.lua")()
 end
