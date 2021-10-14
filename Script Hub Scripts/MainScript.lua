c3e473d5967c5f4578fa95fdab86c67c15ffbbfb
DuckLackn <89722816+DuckLackn@users.noreply.github.com>
2021-10-14 15:35:20 -0400

    Create MainScript.lua

---
+++ Script Hub Scripts/MainScript.lua
@@ -0,0 +1,7 @@
+if game.PlaceId ~= 2377868063 then
+    game:GetService("Players").LocalPlayer:Kick("ur not in jailbreak")
+    return
+end
+if game.PlaceId == 2377868063 then
+    loadstring(game:HttpGet"URL")
+end
