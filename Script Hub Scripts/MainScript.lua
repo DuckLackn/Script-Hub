ccb48497573cdc63cede0e7b210044b5472a9bfe
DuckLackn <89722816+DuckLackn@users.noreply.github.com>
2021-10-15 23:19:25 -0400

    Update MainScript.lua

--- Script Hub Scripts/MainScript.lua
+++ Script Hub Scripts/MainScript.lua
@@ -5,7 +5,7 @@ local SuperDarts = 4914988106
 if game.PlaceId == Strucid then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/DuckLackn/Script-Hub/main/Script%20Hub%20Scripts/Strucid%20Game.lua")()
  elseif game.PlaceId == SuperDarts then
-    print("Worked")
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/DuckLackn/Script-Hub/main/Script%20Hub%20Scripts/Super%20Darts%20Game")()
  elseif game.PlaceId ~= Strucid or SuperDarts then
     game:GetService("Players").LocalPlayer:Kick("This Isn't A Valid Game")
  end
